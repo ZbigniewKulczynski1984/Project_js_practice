38a122c15ae538948f266d3cc1f94ff6c1fb911d
Zbigniew Kulczński <z-kulczynski@o2.pl>
2022-07-26 14:33:10 +0200

    main.js modified

--- main.js
+++ main.js
@@ -4,6 +4,10 @@ const img = document.querySelector('.item1')
 
 const showImg = () => {
     img.classList.toggle('hide')
+    if(img.classList.contains('hide')) {
+        arrowIcon.style.transform = 'rotate(180deg)'
+    }
 }
 
 arrowBtn.addEventListener('click', showImg)
+
