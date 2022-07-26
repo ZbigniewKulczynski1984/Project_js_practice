220c052be6df37e59b3afe728665a675a15ef6a3
Zbigniew Kulczński <z-kulczynski@o2.pl>
2022-07-26 12:26:47 +0200

    script js modified

--- script.js
+++ script.js
@@ -10,6 +10,26 @@ let FavMeal = 'Spagetti'
 
 console.log(`Cześć, mam na imię ${name}, mam ${age} lata i mój ulubiony kolor to ${color}, a ulubiony posiłek to ${FavMeal}.`);
 
+const text = '123'
+    console.log(typeof text);
+
+let userName = "Zbyszek"
+
+let colors = [123,'abcd', true]
+    console.log(color);
+
+function test() {
+    console.log('Cześć, jestem w funkcji')
+}
+test()
+
+const person = {
+    name: 'Klaudia',
+    age: 23,
+    favColor: null
+}
+console.log(person)
+
 
 
 
