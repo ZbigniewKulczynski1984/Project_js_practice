b48a043a7096ed943a4ca02f788d18ca4bae0584
Zbigniew Kulczński <z-kulczynski@o2.pl>
2022-07-26 12:50:43 +0200

    script.js modified

--- script.js
+++ script.js
@@ -1,57 +1,74 @@
 // zmienne
 
-const color = 'niebieski'
-let age = 38
- console.log(color);
- console.log(age);
+// const color = 'niebieski'
+// let age = 38
+//  console.log(color);
+//  console.log(age);
 
-const name = 'Zbyszek'
-    console.log(name);
+// const name = 'Zbyszek'
+//     console.log(name);
 
-let FavMeal = 'Spagetti'
+// let FavMeal = 'Spagetti'
 
-console.log(`Cześć, mam na imię ${name}, mam ${age} lata i mój ulubiony kolor to ${color}, a ulubiony posiłek to ${FavMeal}.`);
+// console.log(`Cześć, mam na imię ${name}, mam ${age} lata i mój ulubiony kolor to ${color}, a ulubiony posiłek to ${FavMeal}.`);
 
-const text = '123'
-    console.log(typeof text);
+// const text = '123'
+//     console.log(typeof text);
 
-let userName = "Zbyszek"
+// let userName = "Zbyszek"
 
-// tablica
-let colors = [123,'abcd', true]
-    console.log(color);
+// // tablica
+// let colors = [123,'abcd', true]
+//     console.log(color);
 
-// funkcja
-function test() {
-    console.log('Cześć, jestem w funkcji')
-}
-test()
+// // funkcja
+// function test() {
+//     console.log('Cześć, jestem w funkcji')
+// }
+// test()
 
 // objekt
-const person = {
-    name: 'Klaudia',
-    age: 23,
-    favColor: null
-}
-console.log(person)
+// const person = {
+//     name: 'Klaudia',
+//     age: 23,
+//     favColor: null
+// }
+// console.log(person)
 
 // operatory atytmetyczne
 
-const x = 5
-const y = 9
+// let x = 5
+// const y = 9
 
-const add = x + y
-console.log(add)
+// const add = x + y
+// console.log(add)
 
-const substract = x - y
-console.log(substract)
+// const substract = x - y
+// console.log(substract)
 
-const multiply = x * y
-console.log(multiply)
+// const multiply = x * y
+// console.log(multiply)
 
-const divide = x / y
-console.log(divide)
+// const divide = x / y
+// console.log(divide)
 
+// x++
+// x--
 
+// const modulo = 10 % 5
+// console.log(modulo)
 
+// operatory przypisania
 
+// let x = 10
+// let y = 5
+
+// console.log(x)
+
+// x = x + y
+// console.log(x)
+
+// x += y
+// console.log(x)
+
+// operatory porównania
